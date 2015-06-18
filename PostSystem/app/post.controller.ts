2040b0a8d61268cdcd9d9c5f824aa5a223633905
pawelhevy <pawelmolek90@gmail.com>
2015-06-18 18:31:49 +0200

    wiele maszyn przechodzenia

--- PostSystem/app/post.controller.ts
+++ PostSystem/app/post.controller.ts
@@ -16,6 +16,16 @@ module post {
         isFirstOpen: boolean;
         isSecondOpen: boolean;
     }
+    
+    class Machine {
+        isEnded: boolean;
+        isSuccessful: boolean;
+        steps: Array<PostModel>;
+
+        constructor() {
+            this.steps = new Array<PostModel>();
+        }
+    }
 
     enum SystemState {
         Start, Stop, Pause
@@ -35,11 +45,13 @@ module post {
         }
         system = {
             state: SystemState.Stop,
-            steps: new Array<PostModel>(),
+            machines: new Array<Machine>(),
             playInterval: -1,
         };
         templates = {
-            "Maszyna akceptująca słowa kończące się na a dla słowa ab": '[{"to":"B0*a*b*B1*","from":"B0","isStarting":true},{"from":"*B1*a","to":"B1*"},{"from":"*B1*b","to":"B2*"},{"from":"*B1*#","to":"B1*"},{"from":"*B1*e","isEnding":true,"to":"B3"},{"from":"*B2","to":"b*B1*"},{"from":"*a","to":"a*"},{"from":"*b","to":"b*"},{"from":"*#","to":"#*"}]'
+            "Maszyna akceptująca słowa kończące się na a dla słowa ab": '[{"to":"B0*a*b*B1*","from":"B0","isStarting":true},{"from":"*B1*a","to":"B1*"},{"from":"*B1*b","to":"B2*"},{"from":"*B1*#","to":"B1*"},{"from":"*B1*e","isEnding":true,"to":"B3"},{"from":"*B2","to":"b*B1*"},{"from":"*a","to":"a*"},{"from":"*b","to":"b*"},{"from":"*#","to":"#*"}]',
+            "Maszyna akceptująca słowa kończące się na a dla słowa a": '[{"to":"B0*a*B1*","from":"B0","isStarting":true},{"from":"*B1*a","to":"B1*"},{"from":"*B1*b","to":"B2*"},{"from":"*B1*#","to":"B1*"},{"from":"*B1*e","isEnding":true,"to":"B3"},{"from":"*B1*B3","isEnding":true,"to":"B3"},{"from":"*B2","to":"b*B1*"},{"from":"*a","to":"a*"},{"from":"*b","to":"b*"},{"from":"*#","to":"#*"}]',
+            "Maszyna akceptująca słowa kończące się na a dla słowa aa": '[{"to":"B0*a*a*B1*","from":"B0","isStarting":true},{"from":"*B1*a","to":"B1*"},{"from":"*B1*b","to":"B2*"},{"from":"*B1*#","to":"B1*"},{"from":"*B1*e","isEnding":true,"to":"B3"},{"from":"*B1*B3","isEnding":true,"to":"B3"},{"from":"*B2","to":"b*B1*"},{"from":"*a","to":"a*"},{"from":"*b","to":"b*"},{"from":"*#","to":"#*"},{"from":"*a*B1","to":"B1*"}]'
         }
 
         public static $inject = ['$scope', '$cookies', 'ngToastr'];
@@ -52,12 +64,13 @@ module post {
             });
             this.restoreStatesFromCookie();
             $scope.$watch(() => { return this.states; }, (oldVal, newVal) => {
-                if (oldVal != newVal && oldVal.length != newVal.length) {
+                if (oldVal != newVal) {
                     this.saveStatesToCookie();
                 }
-            });
+            }, true);
         }
 
+        //#region createPostModel
         restoreStatesFromCookie = () => {
             var states = <Array<PostModel>>this.$cookies.getObject("states");
             if (!!states && states.length > 0) {
@@ -107,7 +120,9 @@ module post {
         hasStates = () => {
             return !!this.states && this.states.length > 0;
         }
+        //#endregion
 
+        //#region engine
         isSystemDisabled = () => {
             return !this.hasStates();
         }
@@ -127,7 +142,7 @@ module post {
         systemStop = () => {
             this.system.state = SystemState.Stop;
             clearInterval(this.system.playInterval);
-            this.system.steps.splice(0, this.system.steps.length);
+            this.system.machines.splice(0, this.system.machines.length);
         }
 
         systemPause = () => {
@@ -151,66 +166,106 @@ module post {
                 this.systemStop();
                 return;
             }
-
-            if (this.system.steps.length == 0) {
-                var startState = this.getStartState();
-                if (!startState) {
-                    this.toastr.getToastr().error("Brak początkowego przejścia");
-                    this.systemStop();
-                    return;
-                }
-                this.system.steps.push(startState);
-            } else {
-                var from = this.getStepSplittedArray(true);
-                var to = this.getStepSplittedArray(false);
-                var matchedStates = [];
-                if (from.length > to.length) {
-                    var toFind = from.slice(to.length).join("");
-                    matchedStates = this.getStateMatched(toFind, false);
-                } else {
-                    var toFind = to.slice(from.length).join("");
-                    matchedStates = this.getStateMatched(toFind, true);
-                }
-                if (!matchedStates || matchedStates.length == 0) {
-                    this.toastr.getToastr().error("Brak przejścia do wykorzystania. Maszyna się zatrzymuje i odrzuca słowo");
-                    return;
-                }
-
-                if (matchedStates.length > 1) {
-                    this.toastr.getToastr().warning("Wiecej niż jedno przejscie do wyboru. Maszyna wybiera pierwsze.");
-                }
-                this.system.steps.push(matchedStates[0]);
+            if (this.system.machines.length == 0) {
+                this.system.machines.push(new Machine());
             }
-            this.systemDetectLoop();
+            var allMachinesEnded = true;
+            angular.forEach(this.system.machines, (machine) => {
+                if (machine.isEnded)
+                    return;
+                allMachinesEnded = false;
+                if (machine.steps.length == 0) {
+                    var startState = this.getStartState();
+                    if (!startState) {
+                        this.toastr.getToastr().error("Brak początkowego przejścia");
+                        this.systemStop();
+                        return;
+                    }
+                    machine.steps.push(startState);
+                } else {
+                    var from = this.getStepSplittedArray(machine.steps, true);
+                    var to = this.getStepSplittedArray(machine.steps, false);
+                    var matchedStates = [];
+                    if (from.length > to.length) {
+                        var toFind = from.slice(to.length).join("");
+                        matchedStates = this.getStateMatched(toFind, false);
+                    } else {
+                        var toFind = to.slice(from.length).join("");
+                        matchedStates = this.getStateMatched(toFind, true);
+                    }
+                    if (!matchedStates || matchedStates.length == 0) {
+                        this.systemPause();
+                        this.toastr.getToastr().error("Brak przejścia do wykorzystania. Maszyna się zatrzymuje i odrzuca słowo");
+                        return;
+                    }
+                    if (matchedStates.length > 1) {
+                        for (var i = 1; i < matchedStates.length; i++) {
+                            var newMachine = new Machine();
+                            newMachine.steps = machine.steps.slice();
+                            newMachine.steps.push(matchedStates[i]);
+                            this.system.machines.push(newMachine);
+                        }
+                    }
+
+                    machine.steps.push(matchedStates[0]);
+                }
+            });
+            if (!allMachinesEnded) {
+                this.systemDetectLoop();
+            }
             this.systemCheckEnd();
         }
 
         systemCheckEnd = () => {
-            var lastStep = this.system.steps[this.system.steps.length - 1];
-            if (!!lastStep && lastStep.isEnding) {
+            var allStepEnded = true;
+            angular.forEach(this.system.machines, (machine) => {
+                if (machine.isEnded)
+                    return;
+                var lastStep = machine.steps[machine.steps.length - 1];
+                if (!!lastStep && lastStep.isEnding) {
+                    machine.isEnded = true;
+                    machine.isSuccessful = true;
+                } else {
+                    allStepEnded = false;
+                }
+            });
+            if (allStepEnded) {
                 this.systemPause();
                 this.toastr.getToastr().success("Maszyna dotarła do końca");
             }
         }
 
         systemDetectLoop = () => {
-            var stepNumbers = this.getStepIdArray();
-            var stepNumersStr = stepNumbers.join("");
-            var subsets = this.getSubsets(stepNumbers);
-            for (var i = 0; i < subsets.length; i++) {
-                var subset = subsets[i];
-                if (subset.length > 1) {
-                    var subsetStr = subsets[i].join("");
-                    var indices = this.getIndicesOf(subsetStr, stepNumersStr, false);
-                    if (indices.length > 1) {
-                        var subStr = subsetStr.repeat(indices.length);
-                        if (stepNumersStr.indexOf(subStr) == stepNumersStr.length - subStr.length) { //ends by lopp
-                            this.systemPause();
-                            this.toastr.getToastr().warning("Wykryto pętlę [" + subsets[i].join(",")+"]. System został zatrzymany");
-                            return;
+            var allStepsEndedOrLooping = true;
+            angular.forEach(this.system.machines, (machine,machineId) => {
+                if (machine.isEnded)
+                    return;
+
+                var stepNumbers = this.getStepIdArray(machine.steps);
+                var stepNumersStr = stepNumbers.join("");
+                var subsets = this.getSubsets(stepNumbers);
+                for (var i = 0; i < subsets.length; i++) {
+                    var subset = subsets[i];
+                    if (subset.length > 1) {
+                        var subsetStr = subsets[i].join("");
+                        var indices = this.getIndicesOf(subsetStr, stepNumersStr, false);
+                        if (indices.length > 1) {
+                            var subStr = subsetStr.repeat(indices.length);
+                            if (stepNumersStr.indexOf(subStr) == stepNumersStr.length - subStr.length) { //ends by lopp
+                                //machine.isEnded = true;
+                                //this.systemPause();
+                                this.toastr.getToastr().warning("Wykryto pętlę [" + subsets[i].join(",") + "] w maszynie "+ (machineId+1));
+                                return;
+                            }
                         }
                     }
                 }
+
+                allStepsEndedOrLooping = false;
+            });
+            if (allStepsEndedOrLooping) {
+                this.toastr.getToastr().warning("Wszystkie możliwe przejścia się zatrzymały bądź się pętlą. System został zatrzymany.");
+                this.systemPause();
             }
         }
 
@@ -218,10 +273,12 @@ module post {
             var matched = [];
             angular.forEach(this.states, (state) => {
                 if (matchInFrom) {
-                    if (match.indexOf(state.from) == 0) {
+                    if (match.indexOf(state.from) == 0 || (state.isEnding && state.from.indexOf(match) == 0 && state.from.substr(match.length) === state.to)) {
+                    //if (match.indexOf(state.from) == 0 || state.from.indexOf(match) == 0) {
                         matched.push(state);
                     }
                 } else {
+                    //if (match.indexOf(state.to) == 0 || state.to.indexOf(match) == 0) {
                     if (match.indexOf(state.to) == 0) {
                         matched.push(state);
                     }
@@ -230,7 +287,7 @@ module post {
             return matched;
         }
 
-        private getStartState = () => {
+        private getStartState = () :PostModel => {
             var startState = null;
             angular.forEach(this.states, (state) => {
                 if (state.isStarting) {
@@ -241,18 +298,18 @@ module post {
             return startState;
         }
 
-        getStepSplittedArray = (getFrom: boolean) => {
+        getStepSplittedArray = (steps: Array<PostModel>, getFrom: boolean) => {
             var array = [];
-            angular.forEach(this.system.steps, (state) => {
+            angular.forEach(steps, (state) => {
                 var toSplit = getFrom ? state.from : state.to;
                 array = array.concat(this.splitState(toSplit));
             });
             return array;
         }
 
-        getStepIdArray = () => {
+        getStepIdArray = (steps: Array<PostModel>) => {
             var array = [];
-            angular.forEach(this.system.steps, (state) => {
+            angular.forEach(steps, (state) => {
                 array.push(this.getStateIndex(state)+1);
             });
             return array;
@@ -323,6 +380,7 @@ module post {
             }
             return indices;
         }
+        //#endregion
     }
 
 
